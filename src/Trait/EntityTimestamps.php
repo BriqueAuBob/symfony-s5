d4c7e56964fefd65a95cad72495bd695be1c14ee
BriqueAuBob <briqueaubob@gmail.com>
2025-02-22 22:38:24 +0100

    bug: return createdAt and updatedAt on contents

--- src/Trait/EntityTimestamps.php
+++ src/Trait/EntityTimestamps.php
@@ -5,15 +5,18 @@ namespace App\Trait;
 use ApiPlatform\Metadata\ApiProperty;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 trait EntityTimestamps
 {
     #[ORM\Column(type: 'datetime')]
     #[ApiProperty(readable: true, writable: false)]
+    #[Groups(['content:read'])]
     public ?DateTime $createdAt = null;
 
     #[ORM\Column(type: 'datetime')]
     #[ApiProperty(readable: true, writable: false)]
+    #[Groups(['content:read'])]
     public ?DateTime $updatedAt = null;
 
     #[ORM\PrePersist]
